27a606455c35172ca5425e31d7b20de9e2394736
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-09 14:35:16 +0600

    footer added

---
+++ src/Pages/Shard/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <footer className="p-5 bg-primary">
+                    <div className="items-center">
+                        <p className="text-3xl text-white">MyToDo</p>
+                        <p className="text-white text-center">Ⓒ MD IBRAHIM</p>
+
+                    </div>
+                </footer>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
